2bc2ef938401bbd2dd5d953d0983a1b57b9a58ac
mariafer26 <157810200+mariafer26@users.noreply.github.com>
2025-09-08 15:05:05 -0500

    Delete

--- Documents/SmartWallet-mafe/smartwallet/smartW/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmartwConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "smartW"
--- Documents/SmartWallet-mafe/smartwallet/smartW/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Saving (models.Model):
-    amount = models.DecimalField(max_digits=20, decimal_places=2)
-    date= models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f"${self.amount}-{self.date.strftime('%Y-%m-%d')}"
